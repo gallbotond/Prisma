3bf9b651b198d92b552d242f9eeb2f47c37268fc
Gáll Botond <gallbotondofficial@gmail.com>
2022-07-19 10:33:03 +0300

    asd

---
+++ Spring/movie-app/src/main/java/com/example/movieapp/mapper/Comment/CreateCommentDto.java
@@ -0,0 +1,51 @@
+package com.example.movieapp.mapper.Comment;
+
+public class CreateCommentDto {
+    Long id;
+    String comment;
+    String date;
+    Long userId;
+    Long movieId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    // Kell egy lebutitott dto csak az idkkal majd a serviceben le kell kezelni a tobbit
+}
